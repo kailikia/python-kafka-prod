b9a580ce5d6bea1b1d7fe4e4648d475ebf563729
kailikia <edwinkailikia@gmail.com>
2025-04-11 21:35:57 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from kafka import KafkaProducer
+
+
+producer = KafkaProducer(
+bootstrap_servers=["localhost:29093"]
+)
+producer.send("my-topic", value="Hello, World!".encode("utf-8"))
+
+# Ensure all buffered messages are sent
+producer.flush()
+
+print("Message sent.")
